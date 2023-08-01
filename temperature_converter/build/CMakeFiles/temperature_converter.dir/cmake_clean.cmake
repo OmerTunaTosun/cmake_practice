0b3078489b44d8486c1b67c5718cf8502f1acf36
OmerTunaTosun <111687773+OmerTunaTosun@users.noreply.github.com>
2023-08-01 19:18:41 +0300

    Add files via upload

---
+++ temperature_converter/build/CMakeFiles/temperature_converter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temperature_converter.dir/converter.cpp.o"
+  "CMakeFiles/temperature_converter.dir/converter.cpp.o.d"
+  "temperature_converter"
+  "temperature_converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temperature_converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ temperature_converter/converter.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    input_type_error:
+    cout << "Input type (c, f, k): ";
+    char input_type;
+    cin >> input_type;
+    if(!(input_type == 'c' || input_type == 'f' || input_type == 'k')) {
+        cout << "Wrong input type.\n";
+        goto input_type_error;
+    }
+    
+    output_type_error:
+    cout << "Output type (c, f, k): ";
+    char output_type;
+    cin >> output_type;
+    if(!(output_type == 'c' || output_type == 'f' || output_type == 'k')) {
+        cout << "Wrong output type.\n";
+        goto output_type_error;
+    }
+    
+    cout << "Temperature value: ";
+    double value;
+    cin >> value;
+    
+    cout << "The new value is: ";
+    if(input_type == output_type)
+        ; //empty statement
+    
+    else if(input_type == 'c')
+        if(output_type == 'f')
+            value = value * 9/5 + 32;  
+        else
+            value += 273.15;
+    
+    else if(input_type == 'f') {
+        value = (value  - 32) * 5/9;
+        if(output_type == 'k')
+            value += 273.15;
+    }
+    
+    else {
+        value -= 273.15;
+        if(output_type == 'f')
+            value = value * 9/5 + 32;
+    }
+    
+    cout << value << " " << char(toupper(output_type)) << '\n';
+}
